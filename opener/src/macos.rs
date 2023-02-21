d620582450db83721055520e4f41560c8ff34fd6
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-02-21 16:04:25 +0100

    Add reveal for macOS

--- opener/src/macos.rs
+++ opener/src/macos.rs
@@ -1,9 +1,24 @@
 use crate::OpenError;
 use std::ffi::OsStr;
+use std::path::Path;
 use std::process::{Command, Stdio};
 
 pub(crate) fn open(path: &OsStr) -> Result<(), OpenError> {
     let mut open = Command::new("open")
+        .arg(path)
+        .stdin(Stdio::null())
+        .stdout(Stdio::null())
+        .stderr(Stdio::piped())
+        .spawn()
+        .map_err(OpenError::Io)?;
+
+    crate::wait_child(&mut open, "open")
+}
+
+pub(crate) fn reveal(path: &Path) -> Result<(), OpenError> {
+    let mut open = Command::new("open")
+        .arg("-R")
+        .arg("--")
         .arg(path)
         .stdin(Stdio::null())
         .stdout(Stdio::null())
