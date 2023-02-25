64ef51d45f6d573dbef8914a36863ca902a61b09
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-02-25 12:55:30 +0100

    Forward output from explorer.exe to null

--- opener/src/windows_and_wsl.rs
+++ opener/src/windows_and_wsl.rs
@@ -1,11 +1,13 @@
 use crate::OpenError;
 use std::path::Path;
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 pub(crate) fn reveal(path: &Path) -> Result<(), OpenError> {
     Command::new("explorer.exe")
         .arg("/select,")
         .arg(path)
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
         .spawn()
         .map_err(OpenError::Io)?;
     Ok(())
