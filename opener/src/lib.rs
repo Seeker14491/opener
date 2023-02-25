49a75e16f73b30fb9c3bb0e96411b76ded41312f
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-02-25 12:40:31 +0100

    Build all features on docs.rs

--- opener/src/lib.rs
+++ opener/src/lib.rs
@@ -1,4 +1,5 @@
 #![doc(html_root_url = "https://docs.rs/opener/0.5.2")]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
 //! This crate provides the [`open`] function, which opens a file or link with the default program
 //! configured on the system:
